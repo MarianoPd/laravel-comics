b11f814fbebabff895b352401693998d159bd43b
MarianoPd <marianopidone@gmail.com>
2022-01-21 15:53:36 +0100

    first commit

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,72 @@
+<footer>
+    <footer>
+        <div>
+          <section class="jumbo">
+            <div class="container">
+              <div class="sub-container left">
+               <div class="list">
+                 <h1>dc comics</h1>
+                 <ul>
+                   <li>Charactrs</li>
+                   <li>Comics</li>
+                   <li>Movies</li>
+                   <li>TV</li>
+                   <li>Games</li>
+                   <li>Videos</li>
+                   <li>News</li>
+                 </ul>
+               </div>
+               <div class="list">
+                 <h1>shop</h1>
+                 <ul>
+                   <li>Shop DC</li>
+                   <li>Shop DC Collectables</li>
+                 </ul>
+               </div>
+               <div class="list">
+                 <h1>dc</h1>
+                 <ul>
+                   <li>Terms Of Use</li>
+                   <li>Terms Of Use</li>
+                   <li>Terms Of Use</li>
+                   <li>Terms Of Use</li>
+                   <li>Terms Of Use</li>
+                   <li>Terms Of Use</li>
+                   <li>Terms Of Use</li>
+                   <li>Terms Of Use</li>
+                   <li>Terms Of Use</li>
+                   <li>Terms Of Use</li>
+                 </ul>
+               </div>
+               <div class="list">
+                 <h1>sites</h1>
+                 <ul>
+                   <li>lorem</li>
+                   <li>lorem</li>
+                   <li>lorem</li>
+                   <li>lorem</li>
+                   <li>lorem</li>
+                 </ul>
+               </div>
+              </div>
+              <div class="sub-container right">
+                <img src="{{asset('img/dc-logo-bg.png')}}" alt="dc-logo-bg"> 
+              </div>
+            </div>
+          </section>
+          <section class="bottom">
+            <div class="container">
+              <button><h3>sign up now!</h3></button>
+              <div class="socials">
+                <h3>follow us</h3>
+                <img src="{{asset('img/footer-facebook.png')}}" alt="">
+                <img src="{{asset('img/footer-twitter.png')}}" alt="">
+                <img src="{{asset('img/footer-youtube.png')}}" alt="">
+                <img src="{{asset('img/footer-pinterest.png')}}" alt="">
+                <img src="{{asset('img/footer-periscope.png')}}" alt="">
+              </div>
+            </div>
+          </section>
+        </div>
+      </footer>
+</footer>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,16 @@
+<header >
+    <div class="container ">
+        <img src="{{asset('img/dc-logo.png')}}" alt="dc-logo">
+        <ul>
+            {{-- <li 
+            v-for="(item,index) in links" :key="index"
+            :class="{active: index === activeLink}"
+            @click="activeLink = index">
+                <a :href="item.url">
+                {{item.text}}
+                </a>
+            </li> --}}
+        
+        </ul>
+    </div>
+  </header>
